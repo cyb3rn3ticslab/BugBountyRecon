71aea52d7dfff2887099757bdb6bb49dbcd4a951
Md Saney Alam <33377661+cyb3rn3ticslab@users.noreply.github.com>
2022-10-08 00:26:29 -0400

    test

--- recon.sh
+++ recon.sh
@@ -8,18 +8,21 @@ echo "This scan was created on $TODAY"
 echo "Creating directory $DIRECTORY. "
 mkdir $DIRECTORY
 
-if [ $2 == "nmap-only" ]; then
-    echo "Start scanning using nmap"
-    # nmap $DOMAIN > $DIRECTORY\nmap_result
-    echo "The results of nmap scan are stored in $DIRECTORY/nmap_recon"
-elif [ $2 == "dirsearch-only" ]; then
-    echo "Start scanning using dirsearch"
-    cd ../dirsearch && python3 dirsearch.py -u $DOMAIN -e php > $DIRECTORY\dirsearch
-    echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."   
-else
-    echo "Start scanning using nmap"
-    # nmap $DOMAIN > $DIRECTORY\nmap_result
-    echo "The results of nmap scan are stored in $DIRECTORY/nmap_recon"
+case $2 in 
+    nmap-only)
+        echo "Start scanning using nmap"
+        # nmap $DOMAIN > $DIRECTORY\nmap_result
+        echo "The results of nmap scan are stored in $DIRECTORY/nmap_recon"
+    ;;    
+    dirsearch-only)
+        echo "Start scanning using dirsearch"
+        cd ../dirsearch && python3 dirsearch.py -u $DOMAIN -e php > $DIRECTORY\dirsearch
+        echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."   
+    ;;    
+    crt-only)
+        echo "Start scanning using nmap"
+        # nmap $DOMAIN > $DIRECTORY\nmap_result
+        echo "The results of nmap scan are stored in $DIRECTORY/nmap_recon"
 
     echo "Start scanning using dirsearch"
     cd ../dirsearch && python3 dirsearch.py -u $DOMAIN -e php > $DIRECTORY\dirsearch
